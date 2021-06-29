0181a8d0e237919b277603bc22a175e8c36d70ab
Sudhakar <sudhakar.change@gmail.com>
2021-06-29 14:25:07 -0400

    Added more validation tests

--- src/test/java/com/example/demo/Demo1Application1Test.java
+++ src/test/java/com/example/demo/Demo1Application1Test.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.demo.endpoint.LcsEndpoint;
 import com.example.demo.service.LcsService;
+import com.example.demo.validator.BaseValidator;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers={LcsEndpoint.class})
@@ -28,11 +29,16 @@ public class Demo1Application1Test {
 
 	@MockBean
 	LcsService lcsService;
+	
+	@MockBean
+	BaseValidator validator;
 
 	// Validation test scenarios
 	private static final String TEST_CASE5_BAD_REQUEST_EMPTY_VALUE = "/case5.json";
 	private static final String TEST_CASE6_BAD_REQUEST_EMPTY_SET_OF_STRINGS = "/case6.json";
-	private static final String TEST_CASE7_BAD_REQUEST_EMPTY_SET_OF_STRINGS = "/case7.json";
+	private static final String TEST_CASE7_BAD_REQUEST_DUPLICATE_STRINGS = "/case7.json";
+	private static final String TEST_CASE8_BAD_REQUEST_FAULTY_FORMAT = "/case8.json";
+	private static final String TEST_CASE9_BAD_REQUEST_EMPTY_BODY = "/case9.json";
 
 	@Test
 	public void test_findLcs_validation() throws Exception {
@@ -56,9 +62,29 @@ public class Demo1Application1Test {
 	public void test_findLcs_validation3() throws Exception {
 		assertNotNull(mockMvc);
 		assertNotNull(lcsService);
-		String json = IOUtils.resourceToString(TEST_CASE7_BAD_REQUEST_EMPTY_SET_OF_STRINGS, Charset.defaultCharset());
+		String json = IOUtils.resourceToString(TEST_CASE7_BAD_REQUEST_DUPLICATE_STRINGS, Charset.defaultCharset());
 		mockMvc.perform(post("/lcs").contentType(MediaType.APPLICATION_JSON_VALUE).content(json))
-				.andExpect(status().isOk());
+				.andExpect(status().isBadRequest());
 	}
+	
+	@Test
+	public void test_findLcs_validation4() throws Exception {
+		assertNotNull(mockMvc);
+		assertNotNull(lcsService);
+		String json = IOUtils.resourceToString(TEST_CASE8_BAD_REQUEST_FAULTY_FORMAT, Charset.defaultCharset());
+		mockMvc.perform(post("/lcs").contentType(MediaType.APPLICATION_JSON_VALUE).content(json))
+				.andExpect(status().isBadRequest());
+	}
+	
+	
+	@Test
+	public void test_findLcs_validation5() throws Exception {
+		assertNotNull(mockMvc);
+		assertNotNull(lcsService);
+		String json = IOUtils.resourceToString(TEST_CASE9_BAD_REQUEST_EMPTY_BODY, Charset.defaultCharset());
+		mockMvc.perform(post("/lcs").contentType(MediaType.APPLICATION_JSON_VALUE).content(json))
+				.andExpect(status().isBadRequest());
+	}
+
 
 }
