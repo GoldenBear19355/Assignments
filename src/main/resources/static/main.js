17872b77f64aaec3cbf74518831ed132896411fa
Sudhakar <sudhakar.change@gmail.com>
2021-06-30 13:49:22 -0400

    Added Jquery UI

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+    $("#search-form").submit(function (event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+// Manual submit
+        post_ajax_submit();
+
+    });
+
+});
+
+function post_ajax_submit() {
+
+    var search = {}
+  
+//window.alert($("#setofstrings").val());
+
+    $("#btn-search").prop("disabled", true);
+    
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "/lcs",
+        data: $("#setofstrings").val(),
+        dataType: 'json',
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+        	
+            var json = "<h4>LCS Response</h4>"
+                + JSON.stringify(data, null, 4);
+            $('#feedback').html(json);
+//window.alert(data);
+            console.log("SUCCESS : ", data);
+            $("#btn-search").prop("disabled", false);
+
+        },
+        error: function (e) {
+
+            var json = "<h4>LCS Response</h4>"
+                + e.responseText;
+            $('#feedback').html(json);
+// window.alert(e);
+            console.log("ERROR : ", e);
+            $("#btn-search").prop("disabled", false);
+
+        }
+    });
+
+}
